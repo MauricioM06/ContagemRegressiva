b5224c328cab0251b9ce20213351dc35c0b2f7df
MauricioM06 <78988280+MauricioM06@users.noreply.github.com>
2021-03-10 13:02:37 -0300

    Add files via upload

---
+++ ContagemRegressiva.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace FOGO
+{
+
+    public class Program 
+    {
+        public static void Main( string [] args )
+        {
+        	Console.WriteLine("Pronto para disparar? Aperte Enter para continuar...");
+        	Console.ReadKey();
+        	Console.Clear();
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Preparar...");
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.ReadKey();
+			Console.WriteLine("Apontar...");
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.ReadKey();
+			Console.WriteLine("FOGO!");
+			Console.ResetColor();
+        }
+    }
+}
